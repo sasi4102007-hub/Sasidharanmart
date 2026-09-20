38212c8854f10c5bd6b205d8f24325256a0626f4
sasi4102007-hub <sasi4102007@gmail.com>
2026-09-20 12:45:53 +0530

    Add files via upload

---
+++ CartItemRepository.java
@@ -0,0 +1,18 @@
+package com.sasidharanmart.shop.repository;
+
+import com.sasidharanmart.shop.model.CartItem;
+import com.sasidharanmart.shop.model.Product;
+import com.sasidharanmart.shop.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    List<CartItem> findByUser(User user);
+
+    Optional<CartItem> findByUserAndProduct(User user, Product product);
+
+    void deleteByUser(User user);
+}
