e918fe9a87bffbfba0a6e3b24675d20cc63167df
sasi4102007-hub <sasi4102007@gmail.com>
2026-09-20 12:49:10 +0530

    Add files via upload

---
+++ SasidharanmartApplication.java
@@ -0,0 +1,16 @@
+package com.sasidharanmart.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for the Sasidharanmart Login & Registration module.
+ * Run this class to start the embedded Tomcat server on port 8080.
+ */
+@SpringBootApplication
+public class SasidharanmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SasidharanmartApplication.class, args);
+    }
+}
